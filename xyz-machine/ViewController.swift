d4387257d7bb75cc34d2c14cab59bcbf12e008d6
Willie Fatimehin <willie.fatimehin@gmail.com>
2019-02-09 21:10:31 -0500

    acceleromater graphics added

--- xyz-machine/ViewController.swift
+++ xyz-machine/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var xLabel: UILabel!
+    @IBOutlet weak var yLabel: UILabel!
+    @IBOutlet weak var zLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
     }
 
 
